94d92b12f81a1990c26329a326daef730843707b
corgvi <vvc2605@gmail.com>
2025-08-07 13:13:22 +0700

    Initial Project

---
+++ src/main/java/com/cvv/scm_link/ScmLinkApplication.java
@@ -0,0 +1,13 @@
+package com.cvv.scm_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvv/scm_link/ScmLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvv.scm_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
